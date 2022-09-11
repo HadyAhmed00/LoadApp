d48a712094babe2e433b635874a60d290ea392f1
Hady Ahmed Abd El-slame <61879050+HadyAhmed00@users.noreply.github.com>
2022-09-11 08:44:33 +0200

    updates

--- app/src/main/java/com/hadyahmed00/loadapp/CustomView.kt
+++ app/src/main/java/com/hadyahmed00/loadapp/CustomView.kt
@@ -1,13 +1,12 @@
 package com.hadyahmed00.loadapp
 
 import android.animation.ValueAnimator
-import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.*
+import android.graphics.drawable.shapes.ArcShape
 import android.util.AttributeSet
 import android.view.View
 import androidx.core.content.withStyledAttributes
-import java.lang.Integer.min
 import kotlin.properties.Delegates
 
 class CustomView @JvmOverloads constructor(
@@ -16,109 +15,112 @@ class CustomView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
-    private var widthSize = 0
-    private var heightSize = 0
-
-    private val valueAnimator = ValueAnimator.ofInt(0, 360).setDuration(2000)
-
-    var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
-
-        when (new) {
-            ButtonState.Loading -> {
-                buttonTextStr = resources.getString(R.string.button_loading)
-
-                valueAnimator.start()
-
-            }
-            ButtonState.Completed -> {
-                buttonTextStr = resources.getString(R.string.button_name)
-                valueAnimator.cancel()
-
-                progress = 0
-            }
-            else -> {}
-        }
-
-        invalidate()
-    }
-
+    var lift = 0.0f
+    var start =0.0f
+    private var width = 0.0f
+    private var height = 0.0f
+    private var textDraw = "Download"
+    private var canvasBackgroundColor = Color.WHITE
     private var buttonBackgroundColor = 0
     private var buttonTextColor = 0
     private var buttonLoadingColor = 0
     private var buttonCircleColor = 0
+    private val backgroundRect = RectF()
+    private val animationRect = RectF()
 
-    private var buttonTextStr = "Download"
-    private var progress = 0
 
-    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        style = Paint.Style.FILL
-        textAlign = Paint.Align.CENTER
-        textSize = 100.0f
-        typeface = Typeface.create("", Typeface.BOLD)
+
+    private val animator = ValueAnimator.ofFloat(0f,360f)
+
+    private var loader =0f
+
+    var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
+
+        if(new == ButtonState.Loading )
+        {
+            textDraw = "Loading"
+            animator.start()
+        }
+        else{
+            textDraw = "Download"
+            animator.cancel()
+            loader = 0f
+        }
+
+        invalidate()
     }
-
     init {
-//         custom attributes
         context.withStyledAttributes(attrs, R.styleable.LoadingButton) {
             buttonBackgroundColor = getColor(R.styleable.LoadingButton_backgroundColor, 0)
             buttonTextColor = getColor(R.styleable.LoadingButton_textColor, 0)
             buttonLoadingColor = getColor(R.styleable.LoadingButton_buttonLoadingColor, 0)
             buttonCircleColor = getColor(R.styleable.LoadingButton_buttonCircleColor, 0)
         }
+        buttonState = ButtonState.Loading
 
-        buttonState = ButtonState.Completed
-
-        // setup animation
-        valueAnimator.apply {
+        animator.apply {
+            duration = 3000L
             addUpdateListener {
-                progress = it.animatedValue as Int
+                loader=it.animatedValue as Float
                 invalidate()
             }
             repeatCount = ValueAnimator.INFINITE
-            repeatMode = ValueAnimator.RESTART
+            repeatMode = ValueAnimator.REVERSE
+        }
+
+
+    }
+
+
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        width = w-lift
+        height = h-start
+        super.onSizeChanged(w, h, oldw, oldh)
+
+    }
+
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        textAlign = Paint.Align.CENTER
+        textSize = 70f
+    }
+
+    private fun getDimensions(canvas: Canvas?) {
+        lift = 10.0f
+        start = 10.0f
+        width = canvas?.width!!.toFloat() - lift
+        height = canvas.height.toFloat() - start
+        backgroundRect.apply {
+            this.top = start
+            this.left = lift
+            this.right = width
+            this.bottom = height
+        }
+
+        animationRect.apply {
+            top = start
+            left = lift
+            right =width * loader/360
+            bottom = height
         }
     }
 
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        val minw: Int = paddingLeft + paddingRight + suggestedMinimumWidth
-        val w: Int = resolveSizeAndState(minw, widthMeasureSpec, 1)
-        val h: Int = resolveSizeAndState(
-            MeasureSpec.getSize(w),
-            heightMeasureSpec,
-            0
-        )
-        widthSize = w
-        heightSize = h
-        setMeasuredDimension(w, h)
-    }
-
     override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
+        getDimensions(canvas)
+        canvas?.drawColor(canvasBackgroundColor)
 
-        // button background
         paint.color = buttonBackgroundColor
-        canvas?.drawRect(0f, 0f, widthSize.toFloat(), heightSize.toFloat(), paint)
+        canvas?.drawRect(backgroundRect, paint)
 
-        // loading button
         paint.color = buttonLoadingColor
-        canvas?.drawRect(0f, 0f, widthSize * progress / 360f, heightSize.toFloat(), paint)
+        canvas?.drawRect(animationRect,paint)
 
-        // text
-        paint.color = buttonTextColor
-        canvas?.drawText(buttonTextStr, widthSize / 2.0f, heightSize / 2.0f + 30.0f, paint)
-
-        // circle
         paint.color = buttonCircleColor
-        canvas?.drawArc(
-            widthSize - 200f,
-            50f,
-            widthSize - 100f,
-            150f,
-            0f,
-            progress.toFloat(),
-            true,
-            paint
-        )
+        canvas?.drawArc(width-200f,50f,width - 100f,150f,0f, loader, true, paint)
+
+        paint.color = buttonTextColor
+        canvas?.drawText(textDraw, (width/2), (height/2)+20.0f,paint)
+
     }
 }
 
